74782eacb47a7172c52bed9544f84ba7a9a892cd
davide <davide@patatoHost>
2017-02-07 13:00:18 +0100

    Added Employee model class

---
+++ src/model/Employee.java
@@ -0,0 +1,119 @@
+package model;
+
+import java.sql.Date;
+
+import javax.persistence.Column;
+import javax.persistence.DiscriminatorColumn;
+import javax.persistence.DiscriminatorType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Employee")
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "contractType", discriminatorType = DiscriminatorType.STRING)
+public class Employee {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	private String firstName;
+	private String lastName;
+	private String paymentMethod;
+	private String bankAccount;
+	private String postalAddress;
+	private int weeklyDues;
+	private boolean inUnion;
+	private Date nextPayment;
+
+	@Column(name = "contractType")
+	private String contractType;
+
+	public Employee() {
+		super();
+	}
+
+	public Employee(String firstName, String lastName) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getPaymentMethod() {
+		return paymentMethod;
+	}
+
+	public void setPaymentMethod(String paymentMethod) {
+		this.paymentMethod = paymentMethod;
+	}
+
+	public String getBankAccount() {
+		return bankAccount;
+	}
+
+	public void setBankAccount(String bankAccount) {
+		this.bankAccount = bankAccount;
+	}
+
+	public String getPostalAddress() {
+		return postalAddress;
+	}
+
+	public void setPostalAddress(String postalAddress) {
+		this.postalAddress = postalAddress;
+	}
+
+	public int getWeeklyDues() {
+		return weeklyDues;
+	}
+
+	public void setWeeklyDues(int weeklyDues) {
+		this.weeklyDues = weeklyDues;
+	}
+
+	public boolean isInUnion() {
+		return inUnion;
+	}
+
+	public void setInUnion(boolean inUnion) {
+		this.inUnion = inUnion;
+	}
+
+	public Date getNextPayment() {
+		return nextPayment;
+	}
+
+	public void setNextPayment(Date nextPayment) {
+		this.nextPayment = nextPayment;
+	}
+
+	public String getContractType() {
+		return contractType;
+	}
+
+	public void setContractType(String contractType) {
+		this.contractType = contractType;
+	}
+
+}
---
+++ src/test/EmployeeTest.java
@@ -0,0 +1,10 @@
+package test;
+
+import org.jboss.arquillian.junit.Arquillian;
+import org.junit.runner.RunWith;
+
+@RunWith(Arquillian.class)
+public class EmployeeTest extends ArquillianTest {
+	
+	
+}
